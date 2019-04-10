0f3bcb2029e78e681c85222e3d3cd7ecbe185a92
crisciano <criscianotsi@gmail.com>
2019-04-10 10:43:12 -0300

    SavePoint

--- projeto/servicos/memcachedClient.js
+++ projeto/servicos/memcachedClient.js
@@ -6,13 +6,27 @@ const options = {
     retry: 10000,
     remove: true
 };
+const data = {
+    "id": 20
+};
 
-const cliente = new Memcached(server, options);
-
-cliente.get('', (err, res)=> {
-    if(err || !res){
-        if(err) console.log(err);
-        if(!res) console.log("res not found");
+class MencachedClient{
+    constructor(){
+        this.cliente = new Memcached(server, options);
     }
-    console.log(res);
-})
+
+    memcachedSet(url, data, callback){
+        this.cliente.set('pagamento-20', data, 60000, callback);
+    }
+    memcachedGet(url, callback){
+        this.cliente.get('pagamento-20', (err, res)=> {
+            if(err || !res){
+                if(err) console.log(err);
+                if(!res) console.log("res not found");
+            }
+            console.log(res);
+        })
+    }
+}
+
+module.exports = MencachedClient
